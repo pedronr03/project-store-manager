75b6097997b066cad6082e57a38b89aaabb3b746
Pedro Reis <pedronr0309@gmail.com>
2022-08-16 10:43:23 -0300

    starting project.

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
 
-// não remova esse endpoint, é para o avaliador funcionar
+app.use(express.json);
+
 app.get('/', (_request, response) => {
   response.send();
 });
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
 module.exports = app;
